f4eefa2060da93974c95a82f2034acf82eb7ed7c
freePerro <dougblumer51@gmail.com>
2023-03-19 20:13:05 -0500

    change to setup and python version

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "OctoPrint>=1.4.0",
+        "OctoPrint>=1.3.0",
         "smbprotocol>=1.6.0",
-        "python_version >= '3.6'",
     ],
+    python_requires=">=3.6",
     entry_points={
         "octoprint.plugin": [
             "SMB_octoprint = SMB_octoprint"
